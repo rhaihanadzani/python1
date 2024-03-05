0329cd06433c6492bafd4b9fb30e617360418a69
rhaihanadzani <130121625+rhaihanadzani@users.noreply.github.com>
2024-03-06 00:36:21 +0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        input_user = int(input("Pilih Nomor: "))
-        print(input_user)
-        break
-    except ValueError:
-        print("Input yang Anda masukkan salah. Harap masukkan bilangan bulat.")
-
-print("haha")
